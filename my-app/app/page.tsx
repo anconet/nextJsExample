0ecec615444bb14cbc1556e10c91e6f444420f1c
Anthony Anconetani <anthony.anconetani@gmail.com>
2023-06-24 16:17:23 -0500

    Initial commit, saving my progress.

---
+++ my-app/app/page.tsx
@@ -0,0 +1,185 @@
+'use client'
+
+import Image from 'next/image'
+import { useState } from "react";
+import { useEffect } from 'react';
+import axios from "axios";
+
+export default function Home() {
+
+  function Anthony() {
+    const [message, setMessage] = useState("Testing");
+
+    useEffect(() => {
+      axios.get(`http://testVSCode:3000/api/hello`)
+        //.then((response) => console.log(response.data.message))
+        //.then (response) => { response.json() })
+        .then((response) => { setMessage(response.data.message) })
+        .catch(() => { setMessage("error") })
+    },
+      []);
+    return <div>{message}</div>;
+  }
+
+  function Card({ cardData, onCardClick }) {
+
+    return <div
+      className="cssAppCard"
+      onClick={onCardClick}>
+      UID: {cardData.uid}
+      <ul>
+        <li>TITLE: {cardData.title}</li>
+        <li>TYPE: {cardData.cardType}</li>
+      </ul>
+    </div>
+  }
+
+  function Release() {
+    const initialCardStack = [
+      { uid: 0, cardType: 'Feature', title: "Feature1" },
+      { uid: 1, cardType: 'NUDD', title: "NUDD1" },
+      { uid: 2, cardType: 'Study', title: "Study1" },
+      { uid: 3, cardType: 'Question', title: "Question1" }
+    ];
+
+    const [releaseList, setReleaseList] = useState(initialCardStack);
+
+    function handleCardClick() {
+      console.log("Got a Card Click", releaseList.length)
+      //const newReleaseList = releaseList.slice();
+      //newReleaseList.push(<Card onCardClick={() => handleCardClick()}></Card>);
+      //newReleaseList.push(<Card onCardClick={() => handleCardClick()}></Card>);
+      //setReleaseList(newReleaseList);
+    }
+
+    //let myString = <p>2</p>;
+    //let myArray = [];
+    //myArray.push(<Card></Card>);
+    //myArray.push(<Card></Card>);
+    return (
+      <div className='cssAppRelease'>
+        {releaseList.map(card => (
+          <Card
+            cardData={card}
+            onCardClick={() => handleCardClick()}>
+          </Card>))}
+      </div>)
+  }
+
+
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div><p>Hello!!</p></div>
+      <Anthony />
+      <Release />
+      <Release />
+
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Get started by editing&nbsp;
+          <code className="font-mono font-bold">app/page.tsx</code>
+        </p>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{' '}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
+
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Docs{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Find in-depth information about Next.js features and API.
+          </p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Learn{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Templates{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Explore the Next.js 13 playground.
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Deploy{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
+    </main>
+  )
+}
